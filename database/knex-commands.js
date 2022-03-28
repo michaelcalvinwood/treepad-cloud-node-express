9c1e66fb6120cd3c2d6e40fb274c3c4e7976e012
Michael Wood <michaelwood33311@icloud.com>
2022-03-28 12:27:35 -0400

    prepared start script for heroku

--- database/knex-commands.js
+++ database/knex-commands.js
@@ -2,20 +2,109 @@
 
 //     https://devhints.io/knex
 //     https://dev.to/hoanganhlam/knex-cheat-sheet-79o
-
 const knex = require('knex')(require('../knexfile').development);
+const bcrypt = require("bcrypt");
 
-exports.createTables = () => {
-    // knex.schema
-    // .createTable('user', (table) => {
-    //     table.increments('id')
-    //     table.string('name')
-    //     table.integer('age')
-    // })
-    // .createTable('user2', (table) => {
-    //     table.increments('id')
-    //     table.string('name')
-    //     table.integer('age')
-    // })
-    // .then(() => console.log('user table created'));
+
+
+/*
+    Branches Code:
+        i: id
+        p: parent
+        c: child (first child)
+        b: before (sibling before)
+        a: after (sibling after)
+        s: status (0 = closed, 1 = open) i.e. closed = do not display children; open = display children
+        l: level (1-5) for indentation
+*/
+
+/* addTree occurs in three steps:
+    1) A new tree is created in trees
+    2) A new branch is created in branches
+    3) The trees.branch_order is updated with initial state of the new branch
+*/
+
+const initializeNewTree = (userId, icon, treeName, treeDesc, color) => {
+    return knex('trees')
+    .insert ({
+        user_id: Number(userId),
+        icon: icon,
+        tree_name: treeName,
+        tree_desc: treeDesc,
+        color: color,
+        branch_order: JSON.stringify([])
+    });
+}
+
+const createNewBranch = treeId => {
+    console.log(`created Tree: ${treeId}`);
+    return knex('branches')
+        .insert ({
+            tree_id: info
+        })
+}
+
+const updateNewTreeWithInitialBranch = info => {
+    const branchId = info[0];
+    console.log('branch added', branchId);
+    return knex('trees')
+    .update ({
+        branch_order: JSON.stringify([{i: branchId, p: 0, c: 0, b:0, a:0, s:0, l:1}])
+    })
+    .where ({
+        tree_id: treeId
+    })
+}
+
+exports.addTree = (req, res) => {
+    let {userId, icon, treeName, treeDesc, color} = req.body;
+
+    if (!userId || !icon || !treeName) {
+        return res.status(400).json({status: 'error', message: 'missing input(s)'}); 
+    }
+
+    if (!treeDesc) treeDesc = '';
+
+    if (!color) color = '#000000';
+
+    initializeNewTree (userId, icon, treeName, treeDesc, color)
+    .then(info => createNewBranch(info))   
+    .then(info => updateNewTreeWithInitialBranch(info))
+    .then(info => {
+        res.status(200).json({status: "success"});
+    })
+    .catch(err => {
+        console.log (`create new tree error`, err);
+        res.status(401).json({status: "error", message: err});
+    })
+}
+
+exports.getTrees = (req, res) => {
+    const {userId} = req.params;
+
+    if (!userId) {
+        return res.status(401).json({status: 'error', message: 'missing userId'}); 
+    }
+
+    knex('trees')
+    .join('users', {'trees.user_id': 'users.user_id'})
+    .select('tree_id', 'users.user_name', 'trees.user_id', 'icon', 'tree_name', 'tree_desc')
+    .where({"trees.user_id": userId})
+    .then (data => res.status(200).json({status: 'success', message: data}))
+    .catch (err => res.status(401).json({status: 'error', message: err}));
+}
+
+exports.getBranches = (req, res) => {
+    const {treeId} = req.params;
+
+    if (!treeId) {
+        return res.status(401).json({status: 'error', message: 'missing treeId'}); 
+    }
+
+    knex('trees')
+    .select('branch_order', 'tree_id')
+    .where({"trees.tree_id": treeId})
+    .then (data => res.status(200).json({status: 'success', message: data}))
+    .catch (err => res.status(401).json({status: 'error', message: err}));
+
 }
---
+++ database/knex-create-tables.js
@@ -0,0 +1,114 @@
+const knex = require('knex')(require('../knexfile').development);
+const bcrypt = require("bcrypt");
+
+const createUsersTable = () => {
+    return knex.schema
+    .createTable('users', (table) => {
+        table.bigincrements('user_id').unsigned().notNullable()
+        table.string('user_name').notNullable()
+        table.string('password').notNullable()
+        table.unique('user_name')
+    })
+    .catch(err => {
+        switch (err.errno) {
+            case 1050:
+                console.log ("users table already exists");
+                break;
+            default:
+                console.log ('create user table error', err.errno);
+        }
+    })
+}
+
+const createTreesTable = () => {
+    return knex.schema
+    .createTable('trees', (table) => {
+        table.bigincrements('tree_id').unsigned().notNullable()
+        table.bigInteger('user_id').unsigned().notNullable().references('user_id').inTable('users').onDelete("CASCADE")
+        table.string('icon', 1024)
+        table.string('color').notNullable()
+        table.string('tree_name').notNullable()
+        table.string('tree_desc', 2048)
+        table.text('branch_order', 'mediumText')
+        table.unique(['user_id', 'tree_name']);
+    })
+    .catch(err => {
+        switch (err.errno) {
+            case 1050:
+                console.log ("trees table already exists");
+                break;
+            default:
+                console.log ('create trees table error', err.errno);
+        }
+    })
+}
+
+const createBranchesTable = () => {
+    console.log ("Creating branches table\n\n\n\n");
+
+    return knex.schema
+    .createTable('branches', table => {
+        table.bigincrements('branch_id').unsigned().notNullable()
+        table.bigInteger('tree_id').unsigned().notNullable().references('tree_id').inTable('trees').onDelete("CASCADE")
+        table.string('branch_name', 2048)
+    })
+    .catch(err => {
+        switch (err.errno) {
+            case 1050:
+                console.log ("branches table already exists");
+                break;
+            default:
+                console.log ('create branches table error', err.errno);
+        }
+    });
+}
+
+const createLeavesTable = () => {
+    return knex.schema
+    .createTable('leaves', table => {
+        table.bigincrements('leaf_id').unsigned().notNullable()
+        table.bigInteger('branch_id').unsigned().notNullable().references('branch_id').inTable('branches').onDelete("CASCADE")
+        table.text('content', 'longtext')
+    })
+    .catch(err => {
+        switch (err.errno) {
+            case 1050:
+                console.log ("leaves table already exists");
+                break;
+            default:
+                console.log ('create leaves table error', err.errno);
+        }
+    });
+}
+
+const addUser = (userName, password) => {
+    return knex('users')
+    .insert ({
+        user_name: userName,
+        password: bcrypt.hashSync(password, 10)
+    })
+    .catch (err => {
+        switch (err.errno) {
+            case 1062:
+                console.log (`${userName} already exists in users table`);
+                break;
+            
+            default:
+                console.log ('addUser error', err);
+        }
+    })
+    
+    // const verified = bcrypt.compareSync('Pa$$w0rd', passwordHash);
+}
+
+exports.createTables = () => {
+
+    createUsersTable()
+    .then(createTreesTable())
+    .then(createBranchesTable())
+    .then(createLeavesTable())
+    .then(() => addUser('admin', "Technologist@33301"))
+    .catch (err => {
+        console.log ("Create Tables Error:", err);
+    })
+}
